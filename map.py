b3851c7970ee0ba6a7b9d6121eb795ae64dd934b
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-12-15 12:15:27 +0100

    Initial commit

---
+++ map.py
@@ -0,0 +1,21 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+
+def nme_catgory(dframe):
+    for name in dframe.nom:
+        print(name)
+
+
+def get_df_data(geojsonfile):
+    dframe = gpd.read_file(geojsonfile)
+    return dframe
+
+
+def draw_fr_map(dframe, color, markersize):
+    dframe.plot(color=color, markersize=markersize)
+    plt.show()
+
+
+df = get_df_data("communes.geojson")
+draw_fr_map(df, 'gray', 1.5)
